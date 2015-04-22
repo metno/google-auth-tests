f899e339e70023b6d6e888db58054280f1c4e835
Arnulf Heimsbakk <arnulf.heimsbakk@met.no>
2015-04-22 15:48:26 +0200

    added comments

--- list_sheets2.py
+++ list_sheets2.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-
 import os
-import math
 import string
+
+# Import authentication libraries
 import oauth2client.file
 import oauth2client.client
 import oauth2client.tools
 
+# Import editing  libraries
 import gdata.gauth
 import gdata.spreadsheets.client
 
 if __name__ == '__main__':
 
+    # Just create ~/.scriptname.secret for oauth2 credentials
     script_name = os.path.basename(__file__).split('.')[0]
     secret_file = os.path.expanduser("~/." + script_name + ".secret")
 
+    # Create empty or get oauth2 credentials
     storage = oauth2client.file.Storage(secret_file)
     credentials = storage.get()
+
+    # If no credentials exists authenticate with google 
     if credentials is None or credentials.invalid:
+
+        # Create Oauth flow
         flow = oauth2client.client.OAuth2WebServerFlow(
             client_id='867946856044-n8n69pnpmm3m79h6sqr7kkfgssju9lpi.apps.googleusercontent.com',
             client_secret='n0I4ljpXxzK45e6OUog5ETRX',
@@ -27,22 +34,43 @@ if __name__ == '__main__':
             redirect_uri=' urn:ietf:wg:oauth:2.0:oob'
             )
 
-        #credentials = run(flow_from_clientsecrets("client_secrets.json", scope=["https://spreadsheets.google.com/feeds"]), storage)
+        # Alternative method with client_id and client_secret in json
+        #credentials = oauth2client.tools.run(
+        #    flow_from_clientsecrets("client_secrets.json", scope=["https://spreadsheets.google.com/feeds"]), 
+        #    storage
+        #    )
+        
+        # Get and store the credentials
         credentials = oauth2client.tools.run(flow, storage)
 
+    # Get a spreadsheet client object
     gd_client = gdata.spreadsheets.client.SpreadsheetsClient()
+
+    # Authenticate 
     gd_client.auth_token = gdata.gauth.OAuth2TokenFromCredentials(credentials)
 
+    # List all spreadsheets
     #print gd_client.get_spreadsheets()
 
-    spreadsheet_key = '1Ua0Ir53h12U2doayNXVBk6M6Q4fzDwaIBJGSh6jqo-Y'
+    # Our Hackaton spreadsheet
+    spreadsheet_key = '1slih48v4Fr7uaZDwFZRQ3U0_KlOhr2IHP1URzTx9ECM'
+
+    # Sheet number one
     worksheet_id = 1
+    
+    # Create the range we're working on
+    #   range = A1:B20 equals with range = R1C1:R20C2
+    cell_query = gdata.spreadsheets.client.CellQuery( range="A1",  return_empty=True )
 
-    cell_query = gdata.spreadsheets.client.CellQuery( range="A40",  return_empty=True )
-    # range = A1:B20 equals with range = R1C1:R20C2
-
+    # Get the cell range from the sheet
     cells = gd_client.GetCells(spreadsheet_key, worksheet_id, q=cell_query)
+
+    # We're working on the only cell in our range
     cell_entry = cells.entry[0]
-    cell_entry.cell.input_value = 'hackatron.met.no'
-    gd_client.update(cell_entry) # This is the call to Google Server to update
+
+    # Set cell value
+    cell_entry.cell.input_value = 'HACKATON!'
+
+    # Update spreadsheet
+    gd_client.update(cell_entry) 
 
